b4a9838e7014dcb2bf7966026a0c648185a06fb7
nothing-available <smtkur31@gmail.com>
2024-06-18 11:23:00 +0530

    Dark & Light Theme setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,24 +2,35 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/provider/theme-provider";
+import { cn } from "@/lib/utils";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "TalkStream",
-  description: "A discord clone",
+   title: "TalkStream",
+   description: "A discord clone",
 };
 
 export default function RootLayout({
-  children,
+   children,
 }: {
-  children: React.ReactNode;
+   children: React.ReactNode;
 }) {
-  return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={font.className}>{children}</body>
-      </html>
-    </ClerkProvider>
-  );
+   return (
+      <ClerkProvider>
+         <html lang="en" suppressHydrationWarning>
+            <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
+               <ThemeProvider
+                  attribute="class"
+                  defaultTheme="dark"
+                  enableSystem
+                  storageKey="theme"
+               >
+                  {children}
+               </ThemeProvider>
+            </body>
+         </html>
+      </ClerkProvider>
+   );
 }
